31de301a5465f7b0c3960d96d479ed3c2cc3884e
Ricardo Beaujon <rbeaujon77@gmail.com>
2022-07-29 01:16:01 +0300

    calender manual done

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { OpenCloseResponseI } from '../model/response.interface';
-//import { Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,21 +12,23 @@ export class ApiService {
 
   url: string = 'https://api.polygon.io/v1/open-close/crypto/';
   apikey:  string = 'HsXGDm0Acw5MuVVMGHM6T_cArLqN8Yfo';
+  apikey2: string = 'BOigQhu1KdTBz4dQbhkgTiJQTIcTq_0p';
 
-  public getOpenCloseBTC(currency: string = "USD", date: string) {
+  public getOpenCloseBTC(currency: string, date: string): Observable <OpenCloseResponseI> {
     const direction: string  = this.url + "BTC/" + currency + "/"+ date + "?adjusted=true&apiKey=" + this.apikey; 
     return this.httpClient.get<OpenCloseResponseI>(direction)
+ 
   }
   public getOpenCloseETH(currency: string = "USD", date: string = new Date().toJSON().slice(0,10).replace(/-/g,'-')) {
     const direction: string  = this.url + "ETH/" + currency + "/"+ date + "?adjusted=true&apiKey=" + this.apikey; 
     return this.httpClient.get<OpenCloseResponseI>(direction)
   }
   public getOpenCloseLTC(currency: string = "USD", date: string = new Date().toJSON().slice(0,10).replace(/-/g,'-')) {
-    const direction: string  = this.url + "LTC/" + currency + "/"+ date + "?adjusted=true&apiKey=" + this.apikey; 
+    const direction: string  = this.url + "LTC/" + currency + "/"+ date + "?adjusted=true&apiKey=" + this.apikey2; 
     return this.httpClient.get<OpenCloseResponseI>(direction)
   }
   public getOpenCloseDOT(currency: string = "USD", date: string = new Date().toJSON().slice(0,10).replace(/-/g,'-')) {
-    const direction: string  = this.url + "DOT/" + currency + "/"+ date + "?adjusted=true&apiKey=" + this.apikey; 
+    const direction: string  = this.url + "DOT/" + currency + "/"+ date + "?adjusted=true&apiKey=" + this.apikey2; 
     return this.httpClient.get<OpenCloseResponseI>(direction)
   }
   public getOpenCloseDOGE(currency: string = "USD", date: string = new Date().toJSON().slice(0,10).replace(/-/g,'-')) {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,16 +18,11 @@ export class AppComponent implements OnInit{
   }; 
   state = {
     currency: 'USD',
-    date: new Date().toJSON().slice(0,10).replace(/-/g,'-'),
-    
+    date: new Date().toJSON().slice(0,10).replace(/-/g,'-')
   }
 
-  selectedCrypto = '';
-    onSelectedCrypto(value:string): void {
-      this.callIP()
-    }
   selectedCurrency = '';
-    onSelectedCurrency(value:string): void {
+    onSelectedCurrency(value:string) {
       this.state.currency =value
       this.callIP()
     }  
@@ -36,7 +31,72 @@ export class AppComponent implements OnInit{
  
   ngOnInit() {
     this.callIP()
+    this.getDate()
+
   }
+
+
+    getDate () {
+
+        // YEAR
+
+        let year_satart = (new Date).getFullYear()-1;
+        let year_end = (new Date).getFullYear(); // current year
+        let year_selected = year_end;
+
+        let optionYear = '';
+
+        for (let i = year_satart; i <= year_end; i++) {
+            let yearSelected = (i === year_selected ? ' selected' : '');
+            optionYear += '<option value="' + i + '"' + yearSelected + '>' + i + '</option>';
+        }
+        const year = document.getElementById("year")
+        if(year){
+          year.innerHTML = optionYear;
+        }
+
+        // MONTH
+
+        let month_satart = (new Date).getMonth()-1;
+        let month_end = (new Date).getMonth(); // current year
+        let month_selected = month_end;
+  
+        let optionMonth = '';
+  
+        for (let i = month_satart; i <= month_end; i++) {
+            let monthSelected = (i === month_selected ? ' selected' : '');
+            optionMonth += '<option value="' + i + '"' + monthSelected + '>' + i + '</option>';
+        }
+        const month = document.getElementById("month")
+        if(month){
+          month.innerHTML = optionMonth;
+        }
+        // Day
+
+        let day_satart = 1;
+        let day_end = 31; // current year
+        let day_selected = (new Date).getDay();
+  
+        let optionDay = '';
+  
+        for (let i = day_satart; i <= day_end; i++) {
+            let daySelected = (i === day_selected ? ' selected' : '');
+            optionDay += '<option value="' + i + '"' + daySelected + '>' + i + '</option>';
+        }
+        const day = document.getElementById("day")
+        if(day){
+          day.innerHTML = optionDay;
+        }
+    }
+    
+  
+  avoid429() {
+  const selecto = document.getElementById("currency") as HTMLSelectElement
+  selecto.disabled = true;
+  setTimeout(()=>{
+    selecto.disabled = false;
+  }, 25000)
+}
 
   getDiff(a: number, b: number): number {
     var decreaseValue = b - a;
@@ -45,18 +105,19 @@ export class AppComponent implements OnInit{
 
   callIP() {
     const { currency, date } = this.state
-    this.apiService.getOpenCloseBTC(currency, date).subscribe((BTC)=>{
+   
+    this.apiService.getOpenCloseBTC(currency, date).subscribe(BTC =>{
       this.openCloseResponse.BTC.open = BTC.open;
       this.openCloseResponse.BTC.close = BTC.close;
       this.openCloseResponse.BTC.diff = parseFloat((this.getDiff(parseFloat(BTC.open),parseFloat(BTC.close))).toFixed(2));
       this.openCloseResponse.BTC.color = this.openCloseResponse.BTC.diff > 0 ? "green" : "red"
     });
-    this.apiService.getOpenCloseETH(currency, date).subscribe((ETH)=>{
+    this.apiService.getOpenCloseETH(currency, date).subscribe(ETH =>{
       this.openCloseResponse.ETH.open = ETH.open;
       this.openCloseResponse.ETH.close = ETH.close;
       this.openCloseResponse.ETH.diff = parseFloat((this.getDiff(parseFloat(ETH.open),parseFloat(ETH.close))).toFixed(2));
       this.openCloseResponse.ETH.color = this.openCloseResponse.ETH.diff > 0 ? "green" : "red"
-    });
+    }); 
     this.apiService.getOpenCloseLTC(currency, date).subscribe((LTC)=>{
       this.openCloseResponse.LTC.open = LTC.open;
       this.openCloseResponse.LTC.close = LTC.close;
@@ -75,7 +136,6 @@ export class AppComponent implements OnInit{
       this.openCloseResponse.DOGE.diff = parseFloat((this.getDiff(parseFloat(DOGE.open),parseFloat(DOGE.close))).toFixed(2));
       this.openCloseResponse.DOGE.color = this.openCloseResponse.DOGE.diff > 0 ? "green" : "red"
     });
-
   }
   
 
